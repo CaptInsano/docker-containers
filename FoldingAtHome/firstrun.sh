b40a57673157cb360330ba4b8d7f8ed05baf139f
CaptInsano <imgonnafail@gmail.com>
2015-01-17 01:20:54 +0000

    remove previous .pid file

--- FoldingAtHome/firstrun.sh
+++ FoldingAtHome/firstrun.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#If fahclient.pid is left behind, remove it
+if [ -f /var/run/fahclient.pid ]; then
+   rm /var/run/fahclient.pid
+else
+   done
+fi
+
 # Check if config exists. If not, copy in the sample config
 if [ -f /config/config.xml ]; then
   chmod 777 /config/config.xml
