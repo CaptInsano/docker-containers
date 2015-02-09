0861d57370bca3cb47f9c96b243d7fb9245d624f
CaptInsano <imgonnafail@gmail.com>
2015-02-09 22:23:10 +0000

    update to run file

--- Aria2WebUI/Aria2-run.sh
+++ Aria2WebUI/Aria2-run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-echo "Starting Aria2"
+echo "Starting Apache2"
 /etc/init.d/apache2 start
-/etc/init.d/aria2c start
+
+if [ -f /config/aria2.conf ]; then
+  echo "Config already exists"
+  chown nobody /config/aria2.conf
+  /etc/init.d/aria2c start
+else
+  echo "Creating blank config file"
+  touch /config/aria2.conf
+  chown nobody /config/aria2.conf
+  echo "Starting Aria2"
+  /etc/init.d/aria2c start
+fi
+
