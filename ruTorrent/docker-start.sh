e9457fcce297cde7407e0e70d12453c09c4cd805
CaptInsano <imgonnafail@gmail.com>
2015-03-11 21:28:16 +0000

    fix .rtorrent.rc location

--- ruTorrent/docker-start.sh
+++ ruTorrent/docker-start.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+
+chown -R nobody:users /config
+chown -R nobody:users /download
+
+# Check if config exists. If not, copy in the sample config
+if [ -f /config/.rtorrent.rc ]; then
+  chown torrent:users /config/.rtorrent.rc
+  chmod 777 /config/.rtorrent.rc
+  echo "Using existing config file."
+
+  else
+
+  echo "Creating config from template."
+  cp  /root/rtorrent.rc /config/.rtorrent.rc
+  chown torrent:users /config/.rtorrent.rc
+  chmod 777 /config/.rtorrent.rc
+fi
+
 set -ex
 
 service nginx start
