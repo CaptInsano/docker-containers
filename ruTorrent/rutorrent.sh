fadfc5e481bf117988b4153bdf3c101c3876869e
CaptInsano <imgonnafail@gmail.com>
2015-03-18 21:35:03 +0000

    start script

--- ruTorrent/rutorrent.sh
+++ ruTorrent/rutorrent.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-set -ex
 
 export HOME=~nobody
 cd "$HOME"
-
-LOCK_FILE="/download/.rtorrent.lock"
-
-
-    # Make sure that only an instance of rtorrent form this container
-    # handling files in download folder
-    if ! flock --nonblock --exclusive 9; then
-        >&2 echo 'Some container are managing download folder'
-        exit 1
-    fi
 
     # rtorrent locking does not play well with docker container
     # since we use external lock, remove silly lock file
